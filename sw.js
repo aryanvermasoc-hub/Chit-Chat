fd560a23d00eaabdcbe436ddb829ae3897223272
aryanvermasoc-hub <aryanvermasoc@gmail.com>
2026-05-21 23:24:45 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -24,7 +24,7 @@ messaging.onBackgroundMessage((payload) => {
 // -----------------------------------------
 
 // Yahan version v2 kar diya gaya hai aur duplicate line hata di hai
-const CACHE_NAME = "chitchat-v1.0.5";
+const CACHE_NAME = "chitchat-v1.0.6";
 const ASSETS_TO_CACHE = [
   "./",
   "./index.html",
