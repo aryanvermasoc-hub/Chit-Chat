b57e77f8223cfca2438bad91cce09ebfe2616cf3
aryanvermasoc-hub <aryanvermasoc@gmail.com>
2026-05-22 22:50:53 +0530

    Update Firebase integration and cache version

--- sw.js
+++ sw.js
@@ -24,7 +24,7 @@ messaging.onBackgroundMessage((payload) => {
 // -----------------------------------------
 
 
-const CACHE_NAME = "chitchat-v23";
+const CACHE_NAME = "chitchat-v25";
 const ASSETS_TO_CACHE = [
   "./",
   "./index.html",
