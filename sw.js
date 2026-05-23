ad99d0baad67762602bb5647f61f8023f60e85c4
aryanvermasoc-hub <aryanvermasoc@gmail.com>
2026-05-23 22:15:31 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -24,7 +24,7 @@ messaging.onBackgroundMessage((payload) => {
 // -----------------------------------------
 
 
-const CACHE_NAME = "chitchat-v34";
+const CACHE_NAME = "chitchat-v35";
 const ASSETS_TO_CACHE = [
   "./",
   "./index.html",
